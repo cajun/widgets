030d018d4c087617b3d3fc244a1e19f5de0bd6e9
cajun <zac@kleinpeter.org>
2009-05-29 06:33:33 -0500

    More Initial Changes

--- widgets.rb
+++ widgets.rb
@@ -50,46 +50,6 @@ class SPhoto < Shoes::Widget
     #  @ro = 0
     #end
   end
-  
-  def convert_to_polaroid( photo, mod )
-    download photo.small_url do |smug|
-      debug smug.response.inspect
-      
-      tmp_image = "tmp/#{photo.image_id}.jpg"
-       File.open( tmp_image, "w" ) do |f|
-         f << smug.response.body
-       end
-       rimage = Magick::Image.read("tmp/#{photo.image_id}.jpg").first
-       rimage.border!(18, 18, "#f0f0ff")
-
-       rimage.background_color = "none"
-
-       amplitude  = rimage.columns * 0.01
-       wavelength = rimage.rows  * 2
-
-       rimage.rotate!(90)
-       rimage = rimage.wave(amplitude, wavelength)
-       rimage.rotate!(-90)
-
-       shadow = rimage.flop
-       shadow = shadow.colorize(1, 1, 1, "gray75")
-       shadow.background_color = "white"
-       shadow.border!(10, 10, "white")
-       shadow = shadow.blur_image(0, 3)
-
-       rimage = shadow.composite(rimage, -amplitude/2, 5, Magick::OverCompositeOp)
-
-       rimage.rotate!(-5 + mod)
-       #rimage.trim! # breakes in shoes it's odd
-
-       name = "images/#{photo.image_id}_#{photo.key}.png"
-       rimage.write( name )
-       15.times do
-         break if( File.exists?( name ) )
-         sleep( 0.5 )
-       end
-     end
-  end
 end
 
 Shoes.app :title => "Widget Test" do
